1bcba8a5e2f61f60a5fc44d4b445dcc005301d44
Chen He <chen.he2@mail.mcgill.ca>
2017-07-28 01:57:30 -0400

    ,

---
+++ Assets/Scripts/Battle/Controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+    cTeams ally = PlayersDatas.GetTeams();
+    public GameObject AllyPanel;
+    public GameObject EnnemiesPanel;
+    public GameObject ship;
+    private void Start()
+    {
+        foreach (cNavis obj in ally.Expedition)
+        {
+            GameObject temp = Instantiate(ship);
+            temp.transform.SetParent(AllyPanel.transform);
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
